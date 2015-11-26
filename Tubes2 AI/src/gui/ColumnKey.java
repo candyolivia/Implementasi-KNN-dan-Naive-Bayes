63dfdfb570c162a69617fb76b8b78be6a1c77d2f
azwaradli <azwar.adli@gmail.com>
2015-11-26 15:18:51 +0700

    add stacked bar chart demo

---
+++ Tubes2 AI/src/gui/ColumnKey.java
@@ -0,0 +1,17 @@
+package gui;
+
+public class ColumnKey implements Comparable<String> {
+	 private String time;
+	 public ColumnKey(String time){
+		 this.time = time;
+	 }
+	 
+	 public int compareTo(String o){
+		 return time.compareTo(o);
+	 }
+	  
+	 @Override
+	 public String toString(){
+		 return this.time;
+	 }
+}
